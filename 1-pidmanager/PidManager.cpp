144abc917435609703fc43e4e2c8ff9dce86d09c
Ricky Chon <rickychon99@gmail.com>
2020-09-17 22:05:56 -0400

    DEV: Add implementation for pid manager functions

---
+++ 1-pidmanager/PidManager.cpp
@@ -0,0 +1,41 @@
+/**
+ * Contains implementations for the PID manager functions
+ *
+ * @file PidManager.cpp
+ * @author Ricky Chon
+ */
+
+#include "PidManager.h"
+
+int PidManager::allocate_map()
+{
+    // Populate the array with initial values of 0 to make all PIDs available
+    for(int i = 0; i < pid_map.size(); i++)
+    {
+        pid_map[i] = 0;
+
+        // If we've reached the end of the array and there were
+        // no failures until this point, we can return 1 for success
+        if(i == pid_map.size() - 1) return 1;
+    }
+    return -1;
+}
+
+int PidManager::allocate_pid()
+{
+    // Allocate a PID to the first vacant spot
+    for(int i = 0; i < pid_map.size(); i++)
+    {
+        if(pid_map[i] == 0)
+        {
+            pid_map[i] = 1;
+            return i + MIN_PID;
+        }
+    }
+    return -1;
+}
+
+void PidManager::release_pid(int pid)
+{
+    if(pid > MIN_PID && pid < MAX_PID) pid_map[pid - MIN_PID] = 0;
+}
