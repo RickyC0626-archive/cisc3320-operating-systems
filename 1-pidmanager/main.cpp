fb61dd3e6e3f833f0db83e9155b81f22a107b8d5
Ricky Chon <rickychon99@gmail.com>
2020-09-18 03:29:05 -0400

    TEST: Add test for all successfull routes
    * Successful map allocation
    * Successful PID allocation
    * Successful PID release

--- 1-pidmanager/main.cpp
+++ 1-pidmanager/main.cpp
@@ -8,21 +8,45 @@
 #include "PidManager.h"
 #include "Diagnostics.h"
 
+void success_all();
+
 Diagnostics d;
-PidManager manager;
 
 int main()
 {
-    printf("Allocate map for use\n");
+    success_all();
+
+    return 0;
+}
+
+void success_all()
+{
+    PidManager manager;
+
+    printf("Test Successful Routes\n====================\n");
+
+    printf("\nAllocate map for use\n");
     d.output_result(PidAction::MapAllocation, manager.allocate_map());
 
     printf("\nAllocate a PID to be used\n");
     int PID = manager.allocate_pid();
     d.output_result(PidAction::PidAllocation, PID);
 
-    printf("\nRelease a PID\n");
+    printf("\nRelease the first PID\n");
     manager.release_pid(PID);
     d.output_result(PidAction::PidRelease, PID);
 
-    return 0;
+    // Allocate PID 4000 times
+    for(int i = 0; i < 4000; i++)
+    {
+        manager.allocate_pid();
+    }
+
+    printf("\nRelease PID 3456\n");
+    manager.release_pid(3456);
+    d.output_result(PidAction::PidRelease, 3456);
+
+    printf("\nAllocate a PID again to fill up the first vacant spot\n");
+    int PID3 = manager.allocate_pid();
+    d.output_result(PidAction::PidAllocation, PID3);
 }
